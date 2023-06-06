1f610cbb5928f79f89f69d73f10d08e165d5cc6d
tylerlincheck <32105773+tylerlincheck@users.noreply.github.com>
2023-06-06 13:36:08 -0500

    Update main.tf

--- terraform/subnet_check/main.tf
+++ terraform/subnet_check/main.tf
@@ -1,1 +1,39 @@
+provider "aws" {
+    region = "us-east-2"
+}
 
+resource "aws_security_group" "new-sg" {
+  name = "my-sec-grp-2"
+  vpc_id      = "vpc-0980463a25ba24a2b"
+  description = "Allow HTTP and SSH traffic via Terraform"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "ec2_instance" {
+    ami = "ami-0521a1ab6cb98215d"
+    count = 1
+    subnet_id = "subnet-08f72eee863a99fe7"
+    instance_type = "t2.micro"
+    key_name = "MyKeyPair"
+    vpc_security_group_ids = ["${aws_security_group.new-sg.id}"]
+} 
